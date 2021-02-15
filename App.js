62a70b20b533360babf356f10477b7fb681a190a
lakhanmandloi <30073416+lakhanmandloi@users.noreply.github.com>
2021-02-15 23:08:22 +0530

    update

--- App.js
+++ App.js
@@ -1,19 +1,57 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useState } from 'react';
+import {
+	Button,
+	DrawerLayoutAndroid,
+	Text,
+	StyleSheet,
+	View,
+} from 'react-native';
 
-export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text> Something</Text>
+const App = () => {
+	const drawer = useRef(null);
+
+	const navigationView = () => (
+		<View style={[styles.container, styles.navigationContainer]}>
+			<Text style={styles.paragraph}>I'm in the Drawer!</Text>
+			<Button
+				title='Close drawer'
+				onPress={() => drawer.current.closeDrawer()}
+			/>
 		</View>
 	);
-}
+
+	return (
+		<DrawerLayoutAndroid
+			ref={drawer}
+			drawerWidth={300}
+			drawerPosition={'left'}
+			renderNavigationView={navigationView}>
+			<View style={styles.container}>
+				<Text style={styles.paragraph}>Some text</Text>
+				<Button
+					title='Open drawer'
+					onPress={() => drawer.current.openDrawer()}
+				/>
+			</View>
+		</DrawerLayoutAndroid>
+	);
+};
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center',
+		padding: 16,
+	},
+	navigationContainer: {
+		backgroundColor: '#ecf0f1',
+	},
+	paragraph: {
+		padding: 16,
+		fontSize: 15,
+		textAlign: 'center',
 	},
 });
+
+export default App;
